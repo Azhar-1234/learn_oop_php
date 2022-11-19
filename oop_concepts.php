68ca05b4da91e455c56350138c23331f46cb4762
Azhar-1234 <azhar64100@gmail.com>
2022-11-19 09:00:56 -0800

    secound commit

---
+++ oop_concepts.php
@@ -0,0 +1,24 @@
+<?php
+/*
+	1.what is oop? 
+		1.1. oop concept
+			a.Inheritance
+				visibiliteisAccess Modifier
+					1.public
+					2.protected
+					3.private
+			b.encapsulation(class encapsulate)
+			c.polymorphism 
+		
+	2.what is clss?
+		a blue print of object
+	3.what is object?
+		real chinary of class
+
+
+	4.Others
+		static
+		:: => user for access Static property,methods,constant
+		$this
+		$variable = new className
+*/
